e32f618ae749a4cecf39330478996cb0f8e87278
Manuel Gomez <manuel@vauxoo.com>
2020-04-21 12:49:56 -0500

    [ADD] New Module

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+#  EL unico valor oblogatorio 'category': 'Theme/Creative',
+{
+	'name': 'Theme Turorial',
+	'description': '',
+	'version': '11.0',
+	'autor': 'Vauxoo',
+	'data': [
+	],
+	'category': 'Theme/Creative',
+	'dependes': [
+		'website',
+	],
+}
