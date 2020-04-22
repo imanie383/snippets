7c4781c25dae407091dab39acc82cc82ea7f7757
Manuel Gomez <manuel@vauxoo.com>
2020-04-21 19:35:19 -0500

    [ADD] New link CSS/LESS

--- __manifest__.py
+++ __manifest__.py
@@ -1,10 +1,12 @@
 #  EL unico valor oblogatorio 'category': 'Theme/Creative',
+# pon el assets al inicio 
 {
 	'name': 'Theme Turorial',
 	'description': '',
 	'version': '11.0',
 	'autor': 'Vauxoo',
 	'data': [
+		'views/assets.xml',
 		'views/layout.xml',
 		'views/pages.xml',
 	],
